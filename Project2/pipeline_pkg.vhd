fb92c63532a5e530742f4da46fbfb498cd63c8e1
Charlamada <pleasebitt@gmail.com>
2025-11-10 12:34:39 -0500

    imported the ads library

--- Project2/pipeline_pkg.vhd
+++ Project2/pipeline_pkg.vhd
@@ -2,6 +2,9 @@ use library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
+library ads;
+use ads.ads_complex_pkg.ads_complex;
+
 package pipeline_package is
     
     type signal_elements is record
